965108b451f4d447716de10da18e9ed454a698b1
Pleun Emmelot <pleunelise.emmelot@coderclass.nl>
2017-11-02 10:30:15 +0100

    opdracht 100 balletjes in progress

--- jsvoorgames.js
+++ jsvoorgames.js
@@ -1,5 +1,27 @@
+var ySize= 400;
+var xSize= 400;
+var bal;
+var ballen = []; //Array waar de ballen in komen te staan
+var aantalBallen = 100;
+
 function setup(){
-  createCanvas(400,400);
+  createCanvas(xSize,ySize);
+
+  for (var i = 0; i < aantalBallen; i++){
+    bal = new Ball(21, 21, 10, 5, 3); //Maak een nieuwe instantie van Bal()
+    ballen.push(bal); //Push voegt de bal aan het einde van het array toe
+  }
+}
+
+function draw(){
+  background(255);
+
+  // ballen.length geeft de lengte van het ballenarray terug
+  for (var i = 0; i < ballen.length; i++){
+    bal = ballen[i]; //Haal een bal uit het array
+    //bal.teken();
+    bal.beweeg();
+  }
 }
 
 function Ball(xx,yy){
@@ -14,36 +36,9 @@ function Ball(xx,yy){
 
     if (this.y > (400-10) || this.y < 0){
     this.speedy = -this.speedy;
-
+  }
     if (this.x > (400-10) || this.x < 0){
     this.speedx = -this.speedx;
-
-    }
   }
-  this.teken = function () {
-
   }
-
- }
-
-
-
-}
-
-var balletje = new Ball(10,20);
-var balletje2 = new Ball(1,200);
-var balletje3 = new Ball(45,333);
-
-
-
-function draw() {
-  background(240);
-  rect(balletje.x,balletje.y, 10,10);
-  rect(balletje2.x,balletje2.y, 10,10);
-  rect(balletje3.x,balletje3.y, 10,10);
-  balletje3.update();
-  balletje2.update();
-  balletje.update();
-
-
-}
+  }
