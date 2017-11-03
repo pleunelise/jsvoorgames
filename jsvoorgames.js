4d5aeda7a8eef831b47845c0e9a207aeb3c0b11c
Pleun Emmelot <pleunelise.emmelot@coderclass.nl>
2017-11-03 10:14:22 +0100

    opdracht 100 balletjes

--- jsvoorgames.js
+++ jsvoorgames.js
@@ -8,37 +8,54 @@ function setup(){
   createCanvas(xSize,ySize);
 
   for (var i = 0; i < aantalBallen; i++){
-    bal = new Ball(21, 21, 10, 5, 3); //Maak een nieuwe instantie van Bal()
+    bal = new Ball(random(20, 380), random(20, 380), random(5, 15), random(1,10), random(1,10)); //Maak een nieuwe instantie van Bal()
     ballen.push(bal); //Push voegt de bal aan het einde van het array toe
   }
 }
 
 function draw(){
   background(255);
-
+  var r = random();
   // ballen.length geeft de lengte van het ballenarray terug
   for (var i = 0; i < ballen.length; i++){
     bal = ballen[i]; //Haal een bal uit het array
-    //bal.teken();
-    bal.beweeg();
+    bal.teken();
+    bal.update();
+    bal.botsCheck(speler);
   }
 }
 
 function Ball(xx,yy){
   this.x = xx;
   this.y = yy;
-  this.speedx = 2;
-  this.speedy = 3;
+  this.speedx = 4;
+  this.speedy = 5;
+
+  var dx = xpos2 - xpos1;
+  var dy = ypos2 - ypos1;
+  if (Math.sqrt(dx*dx + dy*dy) <= radius1 + radius2){
+  // Botsing!
+}
+
+  this.botsCheck = function(speler) {
+  // this.xpos geeft de xpos van het huidige balletje
+  // bal.xpos geeft de xpos van het balletje dat je meegeeft als argument
+  ...
+}
+
+  this.teken = function() {
+    ellipse(this.x, this.y,10, 10);
+  }
 
   this.update = function(){
     this.x += this.speedx;
     this.y += this.speedy;
 
-    if (this.y > (400-10) || this.y < 0){
+    if (this.y > (ySize-10) || this.y < 0){
     this.speedy = -this.speedy;
   }
-    if (this.x > (400-10) || this.x < 0){
+    if (this.x > (xSize-10) || this.x < 0){
     this.speedx = -this.speedx;
   }
   }
-  }
+}
